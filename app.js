81ea9263864afbcc429ef0edaad0673517d94e54
anthonyloera2000 <42457669+anthonyloera2000@users.noreply.github.com>
2021-10-18 16:09:21 -0700

    Api

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+const port = 8080;
+const axios = require("axios");
+var cors = require("cors");
+
+app.use(cors());
+
+app.get("/posts", async (request, response) => {
+  try {
+    const results = await axios.get(
+      "https://jsonplaceholder.typicode.com/posts"
+    );
+    response.json(results.data);
+  } catch (error) {
+    console.log(error.repsonse);
+  }
+});
+
+app.get("/posts/:postId", async (request, response) => {
+  const results = await axios.get(
+    `https://jsonplaceholder.typicode.com/posts/${request.params.postId}`
+  );
+  response.json(results.data);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
